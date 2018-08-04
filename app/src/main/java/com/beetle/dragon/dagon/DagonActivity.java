a1c6d8d0e43869bd854f58b4efdeaacfc652cfa6
MakusuMax <40814622+vloxomire@users.noreply.github.com>
2018-08-04 14:28:51 -0300

    Initial commit

---
+++ app/src/main/java/com/beetle/dragon/dagon/DagonActivity.java
@@ -0,0 +1,13 @@
+package com.beetle.dragon.dagon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DagonActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dagon);
+    }
+}
