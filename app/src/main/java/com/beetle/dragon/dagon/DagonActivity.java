4db821fd721301942fb54c6f6ac1cda5d7904369
Max <vloxomire@gmail.com>
2018-08-05 15:32:05 -0300

    Variables
    Se declararon las variables

--- app/src/main/java/com/beetle/dragon/dagon/DagonActivity.java
+++ app/src/main/java/com/beetle/dragon/dagon/DagonActivity.java
@@ -2,8 +2,15 @@ package com.beetle.dragon.dagon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class DagonActivity extends Activity {
+    //Definimos variables en java donde almacenamos las referencias a los objetos definidos en los archivos xml
+    EditText eText1, eText2;
+    Button button; //el unico nombre de variable q toma los tipo Button son button?
+    TextView tView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
