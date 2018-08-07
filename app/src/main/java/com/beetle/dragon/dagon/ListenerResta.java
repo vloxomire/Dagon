a4fa740e411eb9ee364ef60a32f8e8f4ee193c93
MakusuMax <40814622+vloxomire@users.noreply.github.com>
2018-08-07 10:26:42 -0300

    metodo onclick

--- app/src/main/java/com/beetle/dragon/dagon/ListenerResta.java
+++ app/src/main/java/com/beetle/dragon/dagon/ListenerResta.java
@@ -4,4 +4,9 @@ import android.view.View;
 
 public class ListenerResta implements View.OnClickListener {
     DagonActivity context;
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
